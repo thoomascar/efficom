1a05eab8e10bfb2676b1b6437cb8339f935d636a
thoomascar <164499986+thoomascar@users.noreply.github.com>
2024-05-21 09:03:14 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,11 +3,11 @@ variable "default_region" {
 }
 
 variable "project" {
-  type = string
+  default = "efficom"
 }
 
 variable "billing_account" {
-  default = "0150B1-EF0744-75430C"
+  default = "mettre valeur billing account"
 }
 
 variable "default_zone" {
@@ -15,5 +15,5 @@ variable "default_zone" {
 }
 
 variable "ssh_keys" {
-  default = "thoomascaroon:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCeCHYtkRjGOblno6JD7CuEErbzmc2f2ue2zZZ0ZPHeCadPhOUJPy7+oQ9z3+QKH/Ye/TL8w1FGVcIu8iAPLanCtjkHKEvKgB4VOod6HtY0opuaLAP8b4BmET1fKVuSWzxRcW5/mDb3LmD52RN/wNxjFeNuWiIktUk6veskXDod6Swmix/b1CmQfbmeu6U+XmvQT7SiV96HgQ+OGW/vjyAI7QJt6+0lnHPEeY53w04HiJkxbKkUaAf6ZZglYDCsE/+9iVAWbadadX8AgPEJT0tp5UjogQQtJb1SWsDsBDiQifv0NR7aFsQ+1eTUI3JFyuXsKCs0sUYaG91eWkYAZEgpESUp25HejU9UVBBfIk35oPpmsPB6Wh7sgPXJniuw74SywHBLxTp8bQ7O4XQENWTk5HIZaiamVHvZNgZE7FmwpM6A8VYWjnpfoffcLDsipMwgx0aP9o64bKICQkfBJ/JE3yYLN9lGG2jXTgKN2ZZ+o9LwoiHgvUMxmdMrBzNHJ3M= thomas@DESKTOP-UULDQBN"
+  default = "user:cle_ssh"
 }
